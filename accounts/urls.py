fb90f18c00bc9ebab6ce2bedb99298f2bea2f128
hussnain46 <iamhussnain46@gmail.com>
2024-07-11 00:30:45 +0500

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,22 @@
+
+
+from django.urls import path
+from . import views 
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('', views.buyer_dashboard, name='buyer_dashboard'),
+    path('seller/', views.seller_dashboard, name='seller_dashboard'), 
+    path('login/', views.custom_login_view, name='custom_login'),
+    path('add_listing/', views.add_listing, name='add_listing'),
+    path('book_listing/<int:listing_id>/', views.book_listing, name='book_listing'),
+    path('edit_listing/<int:listing_id>/', views.edit_listing, name='edit_listing'),
+    path('delete_listing/<int:listing_id>/', views.delete_listing, name='delete_listing'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('listing/<int:listing_id>/', views.listing_detail_view, name='listing_detail'),
+    path('contact/', views.contact_us, name='contact_us'),
+    path('success/', views.success_page, name='success_page'),
+    path('search/', views.search_listings, name='search_listings'),
+    path('profile/', views.profile_view, name='profile'),
+]
